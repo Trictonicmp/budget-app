c8de1c4ec79e44ffb728cac7fcbdc8ccf5180b29
Trictonicmp <code_shadow@hotmail.com>
2022-09-05 18:37:43 -0500

    Add tests to models

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  
+  let(:user) do
+    User.new(
+      name: 'John'
+      )
+    end
+    
+    let(:category) do
+      Category.new(
+        user: user,
+        name: 'Cat 1',
+        icon_url: 'https://i.pinimg.com/474x/57/9b/28/579b286a34070bf119f5f5959f2d2536.jpg'
+      )
+    end
+    
+  before { 
+    user.save
+    category.save
+  }
+
+  describe 'Category validations' do
+    it 'should have a name' do
+      expect(category).to be_valid
+    end
+
+    it 'is not be valid if name does not exists' do
+      category.name = nil
+      expect(category).to_not be_valid
+    end
+
+    it 'should have an icon_url' do
+      expect(category).to be_valid
+    end
+    
+    it 'is not valid if icon_url does not exists' do
+      category.icon_url = nil
+      expect(category).to_not be_valid
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:author) do
+    User.new(
+      name: 'Amaury Permer',
+    )
+  end
+
+  before { author.save }
+
+  describe 'User validations: ' do
+    it 'Name should be set thus not be valid' do
+      author.name = nil
+      expect(author).to_not be_valid
+    end
+
+    it 'Name should be more than 1 letter thus be valid' do
+      author.name = 'a'
+      expect(author).to be_valid
+    end
+
+
+    it 'Name should be changed to "Mario"' do
+      author.name = 'Mario'
+      name = author.name
+      expect(name).to eql('Mario')
+    end
+  end
+end
