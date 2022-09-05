24f3e659a433c51d3a495eff7a63808585a7f886
Trictonicmp <code_shadow@hotmail.com>
2022-09-05 17:40:02 -0500

    Add expense validation

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :category_expenses, dependent: :delete_all
   has_many :categories, through: :category_expenses
+
+  validates :name, presence: { allow_blank: false, message: 'must not be blank.' }
+  validates :amount, comparison: { greater_than_or_equal_to: 0 }
 end
