33b0ac73390d61aba824fa778f6a58585c852324
Trictonicmp <code_shadow@hotmail.com>
2022-09-06 14:41:48 -0500

    Set initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :categories do
+    resources :expenses
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
