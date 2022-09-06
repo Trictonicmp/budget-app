188f241eb7ec446071d6cff3ec2829209b1ac8b2
Trictonicmp <code_shadow@hotmail.com>
2022-09-06 15:36:37 -0500

    Fix models associations

--- app/models/category_expense.rb
+++ app/models/category_expense.rb
@@ -1,6 +1,6 @@
 class CategoryExpense < ApplicationRecord
-  belongs_to :expenses, class_name: :Expense, optional: true
-  belongs_to :categories, class_name: :Category, optional: true
+  belongs_to :expense, optional: true
+  belongs_to :category, optional: true
 
   validates :exp_id, presence: { allow_blank: false, message: 'must not be blank.' }
   validates :cat_id, presence: { allow_blank: false, message: 'must not be blank.' }
