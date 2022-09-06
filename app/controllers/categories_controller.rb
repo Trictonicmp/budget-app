fe37f97e12812566f7f374d9398af0ba21893ced
Trictonicmp <code_shadow@hotmail.com>
2022-09-06 16:43:19 -0500

    Add category show view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @categories = Category.includes(:expenses).where(user_id: current_user.id)
+  end
+
+  def show
+    @category = Category.includes(:expenses).find(params[:id])
   end
 end
