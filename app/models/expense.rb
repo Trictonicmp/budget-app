c37e03ce6a693e3bd5350bfcb8ec746411ab7e51
Trictonicmp <code_shadow@hotmail.com>
2022-09-07 13:16:17 -0500

    Add nested attributes for category_expense to be able to select a category for the given expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :category_expense
   has_many :categories, through: :category_expense
+  accepts_nested_attributes_for :category_expense
 
   validates :name, presence: { allow_blank: false, message: 'must not be blank.' }
   validates :amount, comparison: { greater_than_or_equal_to: 0 }
