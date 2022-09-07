4fa106c86d8882913769ed597c797a18c79e5b48
Trictonicmp <code_shadow@hotmail.com>
2022-09-06 22:31:17 -0500

    Remove validation for icon_url

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :category_expenses
   has_many :expenses, through: :category_expenses
+  has_one_attached :icon
 
   validates :name, presence: { allow_blank: false, message: 'must not be blank.' }
-  validates :icon_url, presence: { allow_blank: false, message: 'must not be blank.' }
 end
