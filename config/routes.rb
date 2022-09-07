5896d72ad545cfa8a08e8148fa4ec0bacca37210
Trictonicmp <code_shadow@hotmail.com>
2022-09-07 12:09:10 -0500

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :categories do
-    resources :expenses
-  end
+  resources :categories 
+  resources :expenses
 
   root "categories#index"
   # Defines the root path route ("/")
