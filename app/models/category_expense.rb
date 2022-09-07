e60f27643de633d226b0edcbdbbf57b2f0b3bac2
Trictonicmp <code_shadow@hotmail.com>
2022-09-07 13:16:59 -0500

    Add validations

--- app/models/category_expense.rb
+++ app/models/category_expense.rb
@@ -2,6 +2,6 @@ class CategoryExpense < ApplicationRecord
   belongs_to :expense, optional: true
   belongs_to :category, optional: true
 
-  validates :exp_id, presence: { allow_blank: false, message: 'must not be blank.' }
-  validates :cat_id, presence: { allow_blank: false, message: 'must not be blank.' }
+  validates :expense_id, presence: { allow_blank: false, message: 'must not be blank.' }
+  validates :category_id, presence: { allow_blank: false, message: 'must not be blank.' }
 end
