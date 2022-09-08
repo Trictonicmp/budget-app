88247ae361057268f5520df3fc6faf0b3aee3461
Trictonicmp <code_shadow@hotmail.com>
2022-09-07 23:41:31 -0500

    Add redirects for home page and login to not allow users to open categories or expenses when not logged int

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_to_hompeage, only: [:index, :show]
+
+  def redirect_to_hompeage
+    redirect_to root_path if !user_signed_in?
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :redirect_to_hompeage
+  before_action :redirect_logged_user
+
+  def redirect_logged_user
+    redirect_to categories_path if user_signed_in?
+  end
+
   def index
   end
 end
